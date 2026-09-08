e82359a87cc4612517adbdbfb084e6dea89bafb2
Fillip <dhquddlf5@gmail.com>
2026-09-08 22:39:06 +0900

    [chore] : Delete Useless .cjs

--- fix_string.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/pages/ProjectDetail.tsx', 'utf8');
-
-// Fix lines 439, 440
-content = content.replace("A\`;\\n", "A\`;\n");
-content = content.replace("EOF (End of Message)\`;\\n", "EOF (End of Message)\`;\n");
-
-fs.writeFileSync('src/pages/ProjectDetail.tsx', content, 'utf8');
